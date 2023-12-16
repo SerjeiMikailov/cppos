a34aa6416ad7dfdf5888828fa61ddbbe2c0437a9
SerjeiMikailov <felipemaders8@gmail.com>
2023-12-16 14:15:36 -0300

    backspace added

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -5,13 +5,13 @@
 #define MAX_INPUT_LENGHT 200
 #define custom_sizeof(type) ((uint32)(sizeof(type)))
 
-uint16* vga_buffer;
+uint16 *vga_buffer;
 uint32 vga_index;
 static uint32 next_line_index = 1;
 uint8 g_fore_color = Kernel::WHITE, g_back_color = Kernel::BLACK;
 int digit_ascii_codes[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
 
-uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) 
+uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color)
 {
   uint16 ax = 0;
   uint8 ah = 0, al = 0;
@@ -30,7 +30,8 @@ uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color)
 void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color)
 {
   uint32 i;
-  for(i = 0; i < BUFSIZE; i++){
+  for (i = 0; i < BUFSIZE; i++)
+  {
     (*buffer)[i] = vga_entry(NULL, fore_color, back_color);
   }
   next_line_index = 1;
@@ -39,7 +40,7 @@ void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color)
 
 void init_vga(uint8 fore_color, uint8 back_color)
 {
-  vga_buffer = (uint16*)VGA_ADDRESS;
+  vga_buffer = (uint16 *)VGA_ADDRESS;
   clear_vga_buffer(&vga_buffer, fore_color, back_color);
   g_fore_color = fore_color;
   g_back_color = back_color;
@@ -47,11 +48,12 @@ void init_vga(uint8 fore_color, uint8 back_color)
 
 void print_new_line()
 {
-  if(next_line_index >= 55){
+  if (next_line_index >= 55)
+  {
     next_line_index = 0;
     clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
   }
-  vga_index = 80*next_line_index;
+  vga_index = 80 * next_line_index;
   next_line_index++;
 }
 
@@ -64,7 +66,8 @@ void print_char(char ch)
 void print_string(const char *str)
 {
   uint32 index = 0;
-  while(str[index]){
+  while (str[index])
+  {
     print_char(str[index]);
     index++;
   }
@@ -85,8 +88,9 @@ void outb(uint16 port, uint8 data)
 char get_input_keycode()
 {
   char ch = 0;
-  while((ch = inb(KEYBOARD_PORT)) != 0){
-    if(ch > 0)
+  while ((ch = inb(KEYBOARD_PORT)) != 0)
+  {
+    if (ch > 0)
       return ch;
   }
   return ch;
@@ -94,18 +98,19 @@ char get_input_keycode()
 
 void wait_for_io(uint32 timer_count)
 {
-  while(1){
+  while (1)
+  {
     asm volatile("nop");
     timer_count--;
-    if(timer_count <= 0)
+    if (timer_count <= 0)
       break;
-    }
+  }
 }
 
 void M_memset(void *ptr, uint8 value, uint32 num_bytes)
 {
   uint8 *byte_ptr = (uint8 *)ptr;
-  for(uint32 i = 0; i < num_bytes; i++)
+  for (uint32 i = 0; i < num_bytes; i++)
   {
     *byte_ptr = value;
     byte_ptr++;
@@ -119,7 +124,7 @@ void sleep(uint32 timer_count)
 
 int strcmp(const char *str1, const char *str2)
 {
-  while(*str1 && *str2 && *str1 == *str2)
+  while (*str1 && *str2 && *str1 == *str2)
   {
     str1++;
     str2++;
@@ -127,62 +132,88 @@ int strcmp(const char *str1, const char *str2)
   return *str1 - *str2;
 }
 
-void test_input() 
+void test_input()
 {
-    char ch = 0;
-    char keycode = 0;
-    char command_buffer[MAX_INPUT_LENGHT];
-    uint32 command_index = 0;
+  char ch = 0;
+  char keycode = 0;
+  char command_buffer[MAX_INPUT_LENGHT];
+  uint32 command_index = 0;
 
-    do {
-        keycode = get_input_keycode();
+  do
+  {
+    keycode = get_input_keycode();
 
-        if (keycode == KEY_ENTER) {
-            print_new_line();
+    if (keycode == KEY_ENTER)
+    {
+      print_new_line();
 
-            // Null-terminate the command buffer
-            command_buffer[command_index] = '\0';
+      // Null-terminate the command buffer
+      command_buffer[command_index] = '\0';
 
-            // Process the command
-            if (strcmp(command_buffer, "CLEAR") == 0) {
-                clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
-                next_line_index = 1;
-                print_string("CppOS");
-                print_new_line();
-            } else if (strcmp(command_buffer, "OS --VERSION")  == 0) {
-                print_string("CppOS v1.1");
-                print_new_line();
-            } else {
-                // Unrecognized command
-                print_string("Unknown command: ");
-                print_string(command_buffer);
-                print_new_line();
-            }
+      // Process the command
+      if (strcmp(command_buffer, "CLEAR") == 0)
+      {
+        clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
+        next_line_index = 1;
+        print_string("CppOS");
+        print_new_line();
+      }
+      else if (strcmp(command_buffer, "OS --VERSION") == 0)
+      {
+        print_string("CppOS v1.1");
+        print_new_line();
+      }
+      else
+      {
+        // Unrecognized command
+        print_string("Unknown command: ");
+        print_string(command_buffer);
+        print_new_line();
+      }
 
-            // Clear the command buffer for the next input
-            command_index = 0;
-            M_memset(command_buffer, 0, sizeof(command_buffer));
-        } else if (keycode != 0) {
-            ch = Keyboard::get_ascii_char(keycode);
+      // Clear the command buffer for the next input
+      command_index = 0;
+      M_memset(command_buffer, 0, sizeof(command_buffer));
+    }
+    else if (keycode == KEY_BACKSPACE)
+    {
+      if (command_index > 0)
+      {
+        // Move the cursor back
+        vga_index--;
 
-            if (command_index < MAX_INPUT_LENGHT - 1) {
-                // Append the character to the command buffer
-                command_buffer[command_index] = ch;
-                command_index++;
+        // Erase the last character on the screen
+        print_char(' ');
 
-                // Display the character on the screen
-                print_char(ch);
-            }
-        }
-        sleep(0x02FFFFFF);
-    } while (ch > 0);
+        // Move the cursor back again
+        vga_index--;
+
+        // Remove the last character from the command buffer
+        command_index--;
+      }
+    }
+    else if (keycode != 0)
+    {
+      ch = Keyboard::get_ascii_char(keycode);
+
+      if (command_index < MAX_INPUT_LENGHT - 1)
+      {
+        // Append the character to the command buffer
+        command_buffer[command_index] = ch;
+        command_index++;
+
+        // Display the character on the screen
+        print_char(ch);
+      }
+    }
+    sleep(0x02FFFFFF);
+  } while (ch > 0);
 }
 
 void kernel_entry()
 {
   init_vga(Kernel::WHITE, Kernel::BLUE);
-  print_string("CppOS Terminal"); 
+  print_string("CppOS Terminal");
   print_new_line();
   test_input();
-
 }
